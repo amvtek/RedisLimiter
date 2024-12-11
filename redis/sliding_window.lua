eab433feae24db69f2bacd9fce125d25beb29fa7
amvtek <devel@amvtek.com>
2024-12-11 17:42:47 +0200

    Simplify script arguments

--- redis/sliding_window.lua
+++ redis/sliding_window.lua
@@ -6,30 +6,25 @@ in the "sliding window" index...
 
 Usage
 -----
-EVAL path/to/sliding_window.lua 1 key at_usec ttl_msec slwsize_ms slwlimit [slwsize_ms slwlimit]*
+EVAL path/to/sliding_window.lua 1 key slwsize_ms slwlimit [slwsize_ms slwlimit]* [extra_ttl_ms]
 
 Parameters
 ----------
 key: string | KEYS[1]
     key of the sorted set to be read & updated
 
-at_usec: integer | ARGV[1]
-    timestamp to use when accessing the sliding window index
-    it is interpreted as the number of microseconds since 1970-01-01 (Unix Epoch)
-    if at_usec is zero, it will be replaced by redis server current time.
-
-ttl_msec: integer | ARGV[2]
-    time to live in millisecond for the "sliding window" index referenced by slwkey.
-
-slwsize_ms: integer | ARGV[3]
+slwsize_ms: integer | ARGV[1]
     sliding window size in millisecond
     the sliding window range is [(at_usec - slwsize_ms) .. at_usec]
 
-slwlimit: integer | ARGV[4]
+slwlimit: integer | ARGV[2]
     maximum number of events in sliding window
 
 It is possible to define & control more than 1 sliding windows by passing additional
 slwsize_ms, slwlimit parameters to the command...
+
+extra_ttl_msec: integer | ARGV[#ARGV]
+    extra time to live in millisecond for the "sliding window" index referenced by slwkey.
 
 Returns
 -------
@@ -66,21 +61,27 @@ local slwkey = KEYS[1]
 
 -- ARGV length shall be an even number larger than 4
 local numargs = #ARGV
-if ((numargs % 2) ~= 0) or (numargs < 4) then
-  return redis.error_reply("Invalid Args Number")
+if (numargs < 2) then
+  return redis.error_reply("Invalid, Args Number < 2 !")
 end
 
--- read at_usec parameter
--- at_usec normally is current redis time converted to usec
--- script allows passing at_usec explicitly to ease testing
-local at_usec = tonumber(ARGV[1])
-if at_usec == 0 then
-  local redis_time = redis.call("TIME")
-  at_usec = redis_time[1] * 1e6 + redis_time[2]
-end
+-- Convert redis TIME to microsecond timestamp
+local redis_time = redis.call("TIME")
+local at_usec = redis_time[1] * 1e6 + redis_time[2]
 
 -- read time to live in millisecond ms
-local ttl_ms = tonumber(ARGV[2])
+-- by default ttl_ms is 1st window duration
+-- but it can be extended by passing extra_ttl_ms as last argument
+local ttl_ms = tonumber(ARGV[1])
+if (numargs % 2) == 1 then
+  local extra_ttl_ms = tonumber(ARGV[numargs])
+  if extra_ttl_ms < 0 then
+    return redis.error_reply("Invalid, extra_ttl_ms < 0 !")
+  end
+  ttl_ms = ttl_ms + extra_ttl_ms
+  numargs = numargs - 1
+end
+
 local ttl_usec = math.floor(ttl_ms * 1000)
 if ttl_usec > at_usec then
   return redis.error_reply("Invalid ttl duration, can not be larger than at time!")
@@ -102,10 +103,10 @@ end
 -- each sliding window condition is defined by 2 numbers slwsize slwlimit
 -- slwsize is the size of the sliding window in ms
 -- slwlimit is the maximum number of 'events'
-local numcond = (numargs / 2) - 1
+local numcond = numargs / 2
 for i=1, numcond do
-  local slwsize_usec = math.floor(tonumber(ARGV[2*i + 1]) * 1000)
-  local slwlimit = tonumber(ARGV[2*i + 2])
+  local slwsize_usec = math.floor(tonumber(ARGV[2*i - 1]) * 1000)
+  local slwlimit = tonumber(ARGV[2*i])
   -- slwstart, slwend are prefixed with '[' so that redis interprets them as inclusive
   local slwstart = string.format("[%016d_000000", at_usec - slwsize_usec)
   local slwend = string.format("[%016d_999999", at_usec)
