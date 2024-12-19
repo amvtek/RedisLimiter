88ad23bc93d18923e5671117b4095d6708c45dcc
amvtek <devel@amvtek.com>
2024-12-19 09:34:06 +0200

    Reformat sliding_window script using the stylua formatter.

--- src/sliding_window.lua
+++ src/sliding_window.lua
@@ -48,12 +48,13 @@ The maximum value of the timestamp we need to consider is approximately
     tsmax = (100y * 365d * 24h * 3600s) * 1E6 = 3.1536E15
     tsmax requires 51.4859 bits < 53 bits
     tsmax requires 16 decimal digits
-]]--
+]]
+--
 
 -- KEYS length shall be 1
 local numkeys = #KEYS
-if (numkeys ~= 1) then
-  return redis.error_reply("Invalid Key Number")
+if numkeys ~= 1 then
+    return redis.error_reply("Invalid Key Number")
 end
 
 -- read sliding window key
@@ -61,8 +62,8 @@ local slwkey = KEYS[1]
 
 -- ARGV length shall be an even number larger than 4
 local numargs = #ARGV
-if (numargs < 2) then
-  return redis.error_reply("Invalid, Args Number < 2 !")
+if numargs < 2 then
+    return redis.error_reply("Invalid, Args Number < 2 !")
 end
 
 -- Convert redis TIME to microsecond timestamp
@@ -74,28 +75,28 @@ local at_usec = redis_time[1] * 1e6 + redis_time[2]
 -- but it can be extended by passing extra_ttl_ms as last argument
 local ttl_ms = tonumber(ARGV[1])
 if (numargs % 2) == 1 then
-  local extra_ttl_ms = tonumber(ARGV[numargs])
-  if extra_ttl_ms < 0 then
-    return redis.error_reply("Invalid, extra_ttl_ms < 0 !")
-  end
-  ttl_ms = ttl_ms + extra_ttl_ms
-  numargs = numargs - 1
+    local extra_ttl_ms = tonumber(ARGV[numargs])
+    if extra_ttl_ms < 0 then
+        return redis.error_reply("Invalid, extra_ttl_ms < 0 !")
+    end
+    ttl_ms = ttl_ms + extra_ttl_ms
+    numargs = numargs - 1
 end
 
 local ttl_usec = math.floor(ttl_ms * 1000)
 if ttl_usec > at_usec then
-  return redis.error_reply("Invalid ttl duration, can not be larger than at time!")
+    return redis.error_reply("Invalid ttl duration, can not be larger than at time!")
 end
 
 -- clamp the sliding windows
 -- note that remstop is prefixed by '(' so that it is excluded from the range
 local remstop = string.format("(%016d_000000", at_usec - ttl_usec)
-redis.call('ZREMRANGEBYLEX', slwkey, '-', remstop)
+redis.call("ZREMRANGEBYLEX", slwkey, "-", remstop)
 
 -- check that the index contains less than 1E6 events
-local zcard = redis.call('ZCARD', slwkey)
+local zcard = redis.call("ZCARD", slwkey)
 if zcard > 999999 then
-  return redis.error_reply("Index is full, contains more than 1E6 events!")
+    return redis.error_reply("Index is full, contains more than 1E6 events!")
 end
 
 -- check all sliding window conditions
@@ -104,17 +105,17 @@ end
 -- slwsize is the size of the sliding window in ms
 -- slwlimit is the maximum number of 'events'
 local numcond = numargs / 2
-for i=1, numcond do
-  local slwsize_usec = math.floor(tonumber(ARGV[2*i - 1]) * 1000)
-  local slwlimit = tonumber(ARGV[2*i])
-  -- slwstart, slwend are prefixed with '[' so that redis interprets them as inclusive
-  local slwstart = string.format("[%016d_000000", at_usec - slwsize_usec)
-  local slwend = string.format("[%016d_999999", at_usec)
-  local numevt = redis.call('ZLEXCOUNT', slwkey, slwstart, slwend)
-  if numevt >= slwlimit then
-    -- as in C we use non zero value to report error
-    return i
-  end
+for i = 1, numcond do
+    local slwsize_usec = math.floor(tonumber(ARGV[2 * i - 1]) * 1000)
+    local slwlimit = tonumber(ARGV[2 * i])
+    -- slwstart, slwend are prefixed with '[' so that redis interprets them as inclusive
+    local slwstart = string.format("[%016d_000000", at_usec - slwsize_usec)
+    local slwend = string.format("[%016d_999999", at_usec)
+    local numevt = redis.call("ZLEXCOUNT", slwkey, slwstart, slwend)
+    if numevt >= slwlimit then
+        -- as in C we use non zero value to report error
+        return i
+    end
 end
 
 -- all sliding window checks were successful
@@ -122,10 +123,10 @@ end
 -- Note that we append current index cardinality to the event
 -- This is because many events can have the same microsecond timestamp
 local evtkey = string.format("%016d_%06d", at_usec, zcard)
-redis.call('ZADD', slwkey, 0, evtkey)
+redis.call("ZADD", slwkey, 0, evtkey)
 
 -- schedule key expiration
-redis.call('PEXPIRE', slwkey, ttl_ms)
+redis.call("PEXPIRE", slwkey, ttl_ms)
 
 -- 0 status means success
 return 0
